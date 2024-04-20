3429f2a75b51adc14d8254aa308883f2ef476a40
JonasB91 <jonas.backas@hotmail.com>
2024-04-21 01:06:32 +0200

    Sparar senaste, lade till bootstrap testar detta

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import './App.css';
 import About from './Components/About';
+import Work from './Components/Work';
+import Skills from './Components/Skills';
+import Contact from './Components/Contact';
+import Hero from './Components/Hero';
 
 function App() {
   return (
-      <>
-        <Navbar />
-        <main>
-          <Hero />
+<Router>
+      <Navbar />
+      <Hero />
+      <main>
+        <section id="about">
           <About />
-        </main>
-      </>
+        </section>
+        <section id="work">
+          <Work />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
+    </Router>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import '../Css/Navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../Css/Navbar.css';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <header className='fixed-header'>
-      <nav className='navbar'>
-          <div className='title'>J.B Portfolio</div>
-            <ul>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+    <header className={`fixed-header ${showMenu ? 'show-menu' : ''}`}>
+      <nav className='navbar navbar-expand-lg navbar-dark'>
+        <div className='container'>
+          <a href="#" className='navbar-brand'>J.B Portfolio</a>
+          <button className='navbar-toggler' type='button' onClick={toggleMenu}>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className={`collapse navbar-collapse ${showMenu ? 'show' : ''}`}>
+            <ul className='navbar-nav ms-auto'>
+              <li className='nav-item'><a className='nav-link' href="#about">About</a></li>
+              <li className='nav-item'><a className='nav-link' href="#work">Project's</a></li>
+              <li className='nav-item'><a className='nav-link' href="#skills">Skills</a></li>
+              <li className='nav-item'><a className='nav-link' href="#contact">Contact</a></li>
             </ul>
+          </div>
+        </div>
       </nav>
     </header>
   )
