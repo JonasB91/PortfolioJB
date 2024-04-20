9f68c84bd359aacadcd8f29284809646077ae5e5
JonasB91 <jonas.backas@hotmail.com>
2024-04-20 23:22:30 +0200

    La till typeAnimation för text i hero, samt håller på med aboutme componenten

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import '../Css/About.css'
+import ProfilePicture from '../Assets/ProfileJB3.png'
 
 const About = () => {
   return (
-    <div className='about-container'>About</div>
+    <div className='about-container'>
+      <div className='about-text'>
+        <h2>About Me</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Nobis saepe quas fugiat vel, praesentium unde magni suscipit 
+          optio perferendis reprehenderit eius aperiam doloremque tenetur, 
+          rerum animi, qui modi asperiores dolorem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Dolores voluptatibus quam sequi minus, accusamus illo excepturi 
+          commodi rerum aliquam nisi nostrum aliquid nulla neque, corrupti 
+          iusto vero tempore soluta ea.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Repellat laborum asperiores illo, illum ut mollitia dignissimos. 
+          Nulla modi sequi fuga doloremque 
+          dignissimos ipsa adipisci saepe est enim doloribus! Dolorem, mollitia.
+        </p>
+      </div>
+      <div className='profile-picture'>
+        <img src={ProfilePicture} alt="Profile Picture" />
+      </div>
+    </div>
   )
 }
 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../Css/Hero.css'
+import { TypeAnimation } from 'react-type-animation';
+
 
 const Hero = () => {
   return (
@@ -8,7 +10,23 @@ const Hero = () => {
     <i style={{ '--clr': '#00ff0a' }}></i>
     <i style={{ '--clr': '#ff0057' }}></i>
     <i style={{ '--clr': '#fffd44' }}></i>
-        <div className='headline'>Hi, I'm Jonas</div>
+      <div className='headline'>
+      <span style={{ color: 'orange', fontSize: '2em' }}>Hello</span><br />
+        <TypeAnimation
+        sequence={[
+          "I'm, Jonas a frontend developer",
+          2000,
+          'Looking for a new career',
+          2000,
+          'Get to know me!',
+          2000
+        ]}
+        wrapper='span'
+        speed={50}
+        style={{ fontSize: '1em', display: 'inline-block'}}
+        repeat={Infinity}
+        />
+      </div>
       </div>
     </div>
   )
