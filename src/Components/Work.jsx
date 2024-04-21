17175acda03774b2227b73942e48f2da510aa80b
JonasB91 <jonas.backas@hotmail.com>
2024-04-21 16:15:33 +0200

    Mockdata för Work Component, to display Project's

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../Css/Work.css'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div className="work-container">
+      <h2>Work</h2>
+      <div className="grid-container">
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+        <div className="grid-item">Coming soon</div>
+      </div>
+    </div>
   )
 }
 
