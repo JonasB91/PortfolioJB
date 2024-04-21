d4333326fe44979bcb89f86bea98a5236b42df25
JonasB91 <jonas.backas@hotmail.com>
2024-04-21 23:27:54 +0200

    Ändrade position på profile bild

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,10 +5,13 @@ import ProfilePicture from '../Assets/ProfileJB2.png'
 const About = () => {
   return (
     <div className='about-container'>
+      <div className='profile-picture'>
+        <img src={ProfilePicture} alt="Profile Picture" />
+      </div>
       <div className='about-text'>
         <h2>About Me</h2>
         <p>
-          Hi, I'm Jonas, A passionate student of frontend development, 
+          A passionate student of frontend development, 
           Constantly seeking to deepen my understanding and refine my skills in crafting immersive digital experiences. 
           Driven by curiosity and fueled by a love for programming, 
           I thrive on discovering innovative ways to build responsive and user-friendly applications.
@@ -18,9 +21,6 @@ const About = () => {
         I'm eagerly looking forward to bringing my expertise to your team, 
         contributing to our collective success in creating extraordinary digital solutions.
         </p>
-      </div>
-      <div className='profile-picture'>
-        <img src={ProfilePicture} alt="Profile Picture" />
       </div>
     </div>
   )
